488d670aaf40b3b2aae07b9cb900afb9f1bd77c0
McKingston01 <mc.sa.kingston@gmail.com>
2024-10-28 17:27:33 -0300

    Puesta en marcha

---
+++ estudiando/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+   hola
+      </BrowserRouter>
+    <h1 className='text-3xl font-poppins text-white text-center'>Hello</h1>
+    
+    
+    </div>
+  )
+}
+
+export default App
